8cd20a800981af7311a1cea9e76f4ac920f503d7
ooghenekaro <114274865+ooghenekaro@users.noreply.github.com>
2023-04-09 22:05:31 +0200

    Update Home.jsx

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -7,6 +7,9 @@ import WidgetLg from "../../components/widgetLg/WidgetLg";
 import { useEffect, useMemo, useState } from "react";
 import axios from "axios";
 
+const axiosInstance = axios.create ({
+  baseURL: process.env.REACT_APP_API_URL,});
+
 export default function Home() {
   const MONTHS = useMemo(
     () => [
@@ -31,7 +34,7 @@ export default function Home() {
   useEffect(() => {
     const getStats = async () => {
       try {
-        const res = await axios.get("/users/stats", {
+        const res = await axiosInstance.get("/users/stats", {
           headers: {
             token:
               "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwZTZmYzQ2NDk0Mjc3MTYwNDg4MmMxNiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYyNTgzMjMxMSwiZXhwIjoxNjI2MjY0MzExfQ.ATXV-1TTWIGyVBttTQSf0erRWjsgZ8jHQv1ZsUixbng",
